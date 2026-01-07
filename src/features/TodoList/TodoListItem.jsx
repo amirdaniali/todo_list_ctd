d1b5448cd9abf3a930b80f05eb4eca0e5b07d053
Amir Daniali <amir@amirdaniali.com>
2026-01-06 21:59:01 -0500

    extracted validity

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,13 +1,26 @@
-export default function TodoListItem({ todo, style, onCompleteTodo }) {
+import {useState} from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel.jsx";
 
-    return (
-        <li style={style}>
-            <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => onCompleteTodo(todo.id)}
-            />
-            {todo.title}
+export default function TodoListItem({ todo, style, onCompleteTodo }) {
+    let [isEditing, setIsEditing] = useState(false);
+    return ( <li style={style}>
+            <form>
+                {isEditing ? (
+                    <TextInputWithLabel value={todo.title}/>
+                ) : (
+                    <>
+                        <label>
+                            <input
+                                type="checkbox"
+                                id={`checkbox${todo.id}`}
+                                checked={todo.isCompleted}
+                                onChange={() => onCompleteTodo(todo.id)}
+                            />
+                        </label>
+                        <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+                    </>
+                )}
+            </form>
         </li>
     );
 }
